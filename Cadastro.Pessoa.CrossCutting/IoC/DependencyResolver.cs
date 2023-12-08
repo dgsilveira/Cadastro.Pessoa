9f7569405f26438a61fb7bbf2280f0e0f0b77fb3
Douglas Silveira <douglasgsilveira@gmail.com>
2023-12-07 22:41:04 -0300

    DI - Denpendency Injection and  IoC - Inversion of Control. // Buld Dependencies - Api

---
+++ Cadastro.Pessoa.CrossCutting/IoC/DependencyResolver.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cadastro.Pessoa.CrossCutting.IoC
+{
+    public static class DependencyResolver
+    {
+        public static void AddDependencyResolver(this IServiceCollection services)
+        {
+            RegisterApplications(services);
+            RegisterRepositories(services);
+        }
+
+        private static void RegisterApplications(IServiceCollection services)
+        {
+        }
+
+        private static void RegisterRepositories(IServiceCollection services)
+        {
+        }
+    }
+}
